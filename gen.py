05cbf025d894dd25dc28d26f2f2c84cfcdacdaf6
César Del Solar <delsolar@gmail.com>
2024-01-24 21:43:58 -0500

    more tweaks

--- gen.py
+++ gen.py
@@ -1,16 +1,17 @@
 import argparse
 import json
 import math
-import sys
 import qrcode
 
 import cairo
 import webcolors
+from gooey import Gooey, local_resource_path
 
 from fetch_tourney import get_tournament
 
-qrcode_urls = set()
-url_uniqueness_trunc = 3
+
+class URLNotUniqueException(Exception):
+    pass
 
 
 # format color
@@ -59,226 +60,259 @@ def create_simple_qr(data, error_correction=qrcode.constants.ERROR_CORRECT_L):
     return img
 
 
-def place_qr_code(ctx, url):
-    qrcode_urls.add(url)
-    qrcode_img = create_simple_qr(url)
-    # Convert QR code to 'RGBA' format
-    qr_rgba = qrcode_img.convert("RGBA")
+class ScorecardCreator:
+    def __init__(self, tourney, show_opponents: bool, show_seeds: bool):
+        self.tourney = tourney
+        self.show_opponents = show_opponents
+        self.show_seeds = show_seeds
+        self.url_uniqueness_trunc = 2
+        self.qrcode_urls = set()
 
-    qr_bytes = bytearray(qr_rgba.tobytes())
+    def reset(self):
+        self.qrcode_urls = set()
 
-    qrsurface = cairo.ImageSurface.create_for_data(
-        qr_bytes, cairo.FORMAT_ARGB32, qr_rgba.width, qr_rgba.height, qr_rgba.width * 4
-    )
+    def place_qr_code(self, ctx, url):
+        self.qrcode_urls.add(url)
+        qrcode_img = create_simple_qr(url)
+        # Convert QR code to 'RGBA' format
+        qr_rgba = qrcode_img.convert("RGBA")
 
-    ctx.save()
-    ctx.translate(495, 10)
-    ctx.scale(0.20, 0.20)
+        qr_bytes = bytearray(qr_rgba.tobytes())
 
-    ctx.set_source_surface(qrsurface, 0, 0)
-    ctx.paint()
-    ctx.restore()
-
-
-def draw_name_and_tourney_header(ctx, player, pidx, tourney_name):
-    player_name = player["id"].split(":")[1]
-    player_rating = player["rating"]
-
-    black = format_color("black")
-    ctx.new_path()
-    ctx.set_font_size(20)
-    ctx.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-    # circle with number
-    ctx.arc(50, 50, 25, 0, 2 * math.pi)
-    ctx.set_line_width(1)
-    ctx.set_source_rgba(*black)
-    ctx.stroke()
-
-    xidx = 45
-    if len(str(pidx + 1)) > 1:
-        xidx = 40
-    ctx.move_to(xidx, 56)
-    ctx.show_text(str(pidx + 1))
-
-    ctx.move_to(360, 56)
-    ctx.show_text(tourney_name)
-
-    ctx.move_to(225, 75)
-    ctx.set_font_size(12)
-    ctx.show_text("Enter scores and view standings (scan with phone):")
-    # line for player and name
-    ctx.set_font_size(20)
-    ctx.move_to(80, 56)
-    ctx.show_text(f"{player_name}  ({player_rating})")
-    ctx.set_font_size(12)
-
-
-def draw_known_pairings(ctx, div, pidx, rect_ht, nrounds):
-    ctx.set_font_size(12)
-    for k, v in div["pairing_map"].items():
-        if pidx in v["players"]:
-            rd = v["round"]
-            first = True
-            for place, pairedidx in enumerate(v["players"]):
-                if pairedidx != pidx:
-                    opp = pairedidx
-                    opp_name = div["players"]["persons"][opp]["id"].split(":")[1]
-                    if place == 0:
-                        first = False
-            rdY = 125 + (rd * rect_ht) - (2 if nrounds == 8 else 0)
-            rdX = 97
-            if len(str(opp + 1)) > 1:
-                rdX = 93
-            ctx.move_to(rdX, rdY)
-            ctx.show_text(str(opp + 1))
-            ctx.move_to(140, rdY)
-            ctx.show_text(opp_name)
-            # Circle 1st or 2nd
-            y = 130 + (rd * rect_ht) + (5 if nrounds == 7 else 0) - 2
-            ctx.new_sub_path()
-            if first:
-                ctx.arc(40, y, 8, 0, 2 * math.pi)
-            else:
-                ctx.arc(70, y, 8, 0, 2 * math.pi)
-            ctx.stroke()
-    ctx.new_path()
-
-
-def draw_row(ctx, i, rect_ht, nrounds, fields):
-    ctx.rectangle(25, 100 + (i * rect_ht), 535, rect_ht)
-    ctx.stroke()
-    ctx.arc(  # circle around player's number
-        100,
-        100 + (i * rect_ht) + (rect_ht / 2),
-        (rect_ht - 4) / 2,
-        0,
-        2 * math.pi,
-    )
-    ctx.stroke()
-    ctx.set_font_size(18)
-
-    ctx.move_to(50, 125 + (i * rect_ht))
-    ctx.show_text(str(i + 1))
-    ctx.move_to(35, 130 + (i * rect_ht) + (5 if nrounds == 7 else 0))
-    ctx.set_font_size(8)
-    ctx.show_text("1st        2nd")
-
-    last_field = len(fields)
-    if i != 0:
-        last_field = len(fields) - 1
-
-    for f in fields[1:last_field]:
-        ctx.move_to(f[0] - 5, 100 + (i * rect_ht))  # 340 to 380 at end
-        ctx.line_to(f[0] - 5, 100 + (i * rect_ht) + rect_ht)
-        ctx.stroke()
-    # Deal with spread box.
-    if i > 0:
-        f = fields[last_field]
-        ctx.move_to(f[0] - 5, 100 + (i * rect_ht))
-        ctx.line_to(f[0] - 5, 100 + (i * rect_ht) + rect_ht / 2)
-        ctx.stroke()
-        ctx.move_to(fields[last_field - 1][0] - 5, 100 + (i * rect_ht) + rect_ht / 2)
-        ctx.line_to(560, 100 + (i * rect_ht) + rect_ht / 2)
-        ctx.stroke()
-        ctx.move_to(
-            fields[last_field - 1][0] - 2, 100 + (i * rect_ht) + 7 * (rect_ht / 8)
+        qrsurface = cairo.ImageSurface.create_for_data(
+            qr_bytes,
+            cairo.FORMAT_ARGB32,
+            qr_rgba.width,
+            qr_rgba.height,
+            qr_rgba.width * 4,
         )
-        ctx.set_font_size(12)
-        ctx.show_text("Cumulative:")
 
-
-def gen_single_player_scorecard(ctx, div, nrounds, meta, pidx, show_opponents):
-    player = div["players"]["persons"][pidx]
-    idtrunc = player["id"][:url_uniqueness_trunc]
-    qrcode_url = f"https://woogles.io{meta['metadata']['slug']}?es={idtrunc}"
-    if qrcode_url in qrcode_urls:
-        raise Exception("url not unique")
-    place_qr_code(ctx, qrcode_url)
-    draw_name_and_tourney_header(ctx, player, pidx, meta["metadata"]["name"])
-
-    # header row
-    ctx.rectangle(25, 80, 535, 20)
-    ctx.stroke()
-
-    fields = [
-        (35, "Round"),
-        (85, "Opponent"),
-        (300, "Won"),
-        (335, "Lost"),
-        (370, "Your Score"),
-        (440, "Opp Score"),
-        (510, "Spread"),
-    ]
-
-    for field in fields:
-        ctx.move_to(field[0], 95)
-        ctx.show_text(field[1])
-
-    # header lines
-    for f in fields[1:]:
-        ctx.move_to(f[0] - 5, 80)
-        ctx.line_to(f[0] - 5, 100)
-
-    rect_ht = 40
-    if nrounds == 8:
-        rect_ht = 35
-    for i in range(nrounds):
-        draw_row(ctx, i, rect_ht, nrounds, fields)
-
-    # Draw known pairings
-    if show_opponents:
-        draw_known_pairings(ctx, div, pidx, rect_ht, nrounds)
-
-
-def gen_scorecard(surface, ctx, div, nrounds, meta, p1, p2, show_opponents):
-    if p1 != p2:
-        for idx, pidx in enumerate([p1, p2]):
-            ctx.save()
-            ctx.translate(0, idx * 396)
-            gen_single_player_scorecard(ctx, div, nrounds, meta, pidx, show_opponents)
-            ctx.restore()
-        surface.show_page()  # Add a new page for the next scorecard
-    else:
         ctx.save()
-        ctx.translate(0, 200)
-        gen_single_player_scorecard(ctx, div, nrounds, meta, p1, show_opponents)
+        ctx.translate(495, 10)
+        ctx.scale(0.20, 0.20)
+
+        ctx.set_source_surface(qrsurface, 0, 0)
+        ctx.paint()
         ctx.restore()
-        surface.show_page()  # Add a new page for the next scorecard
 
-    surface.flush()
+    def draw_name_and_tourney_header(self, ctx, player, pidx, tourney_name):
+        player_name = player["id"].split(":")[1]
+        player_rating = player["rating"]
 
+        black = format_color("black")
+        ctx.new_path()
+        ctx.set_font_size(20)
+        ctx.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+        player_name_x = 25
+        if self.show_seeds:
+            # circle with number
+            ctx.arc(50, 50, 25, 0, 2 * math.pi)
+            ctx.set_line_width(1)
+            ctx.set_source_rgba(*black)
+            ctx.stroke()
 
-def gen_scorecards(tourney, show_opponents):
-    for divname, div in tourney["t"]["divisions"].items():
-        nrounds = len(div["round_controls"])
-        skip = 2 if nrounds <= 8 else 1
-        fname = f"{divname}_scorecards.pdf"
-        # 8.5 x 11 inches in points (612 x 792)
-        surface = cairo.PDFSurface(fname, 612, 792)
-        ctx = cairo.Context(surface)
+            xidx = 45
+            if len(str(pidx + 1)) > 1:
+                xidx = 40
+            ctx.move_to(xidx, 56)
+            ctx.show_text(str(pidx + 1))
+            player_name_x = 80
 
-        for i in range(0, len(div["players"]["persons"]), skip):
-            gen_scorecard(
-                surface,
-                ctx,
-                div,
-                nrounds,
-                tourney["meta"],
-                i,
-                i + skip - 1,
-                show_opponents,
+        ctx.move_to(360, 56)
+        ctx.show_text(tourney_name)
+
+        ctx.move_to(225, 75)
+        ctx.set_font_size(12)
+        ctx.show_text("Enter scores and view standings (scan with phone):")
+        # line for player and name
+        ctx.set_font_size(20)
+        ctx.move_to(player_name_x, 56)
+        ctx.show_text(f"{player_name}  ({player_rating})")
+        ctx.set_font_size(12)
+
+    def draw_known_pairings(self, ctx, div, pidx, rect_ht, nrounds):
+        ctx.set_font_size(12)
+        for k, v in div["pairing_map"].items():
+            if pidx in v["players"]:
+                rd = v["round"]
+                first = True
+                for place, pairedidx in enumerate(v["players"]):
+                    if pairedidx != pidx:
+                        opp = pairedidx
+                        opp_name = div["players"]["persons"][opp]["id"].split(":")[1]
+                        if place == 0:
+                            first = False
+                rdY = 125 + (rd * rect_ht) - (2 if nrounds == 8 else 0)
+                rdX = 97
+                if len(str(opp + 1)) > 1:
+                    rdX = 93
+                if self.show_seeds:
+                    ctx.move_to(rdX, rdY)
+                    ctx.show_text(str(opp + 1))
+                    ctx.move_to(140, rdY)
+                else:
+                    ctx.move_to(rdX, rdY)
+                ctx.show_text(opp_name)
+                # Circle 1st or 2nd
+                y = 130 + (rd * rect_ht) + (5 if nrounds == 7 else 0) - 2
+                ctx.new_sub_path()
+                if first:
+                    ctx.arc(40, y, 8, 0, 2 * math.pi)
+                else:
+                    ctx.arc(70, y, 8, 0, 2 * math.pi)
+                ctx.stroke()
+        ctx.new_path()
+
+    def draw_row(self, ctx, i, rect_ht, nrounds, fields):
+        ctx.rectangle(25, 100 + (i * rect_ht), 535, rect_ht)
+        ctx.stroke()
+        if self.show_seeds:
+            ctx.arc(  # circle around player's number
+                100,
+                100 + (i * rect_ht) + (rect_ht / 2),
+                (rect_ht - 4) / 2,
+                0,
+                2 * math.pi,
             )
-        surface.finish()
+            ctx.stroke()
+
+        # Round number
+        ctx.set_font_size(18)
+        ctx.move_to(50, 125 + (i * rect_ht))
+        ctx.show_text(str(i + 1))
+        ctx.move_to(35, 130 + (i * rect_ht) + (5 if nrounds == 7 else 0))
+        ctx.set_font_size(8)
+        ctx.show_text("1st        2nd")
+
+        last_field = len(fields)
+        if i != 0:
+            last_field = len(fields) - 1
+
+        for f in fields[1:last_field]:
+            ctx.move_to(f[0] - 5, 100 + (i * rect_ht))  # 340 to 380 at end
+            ctx.line_to(f[0] - 5, 100 + (i * rect_ht) + rect_ht)
+            ctx.stroke()
+        # Deal with spread box.
+        if i > 0:
+            f = fields[last_field]
+            ctx.move_to(f[0] - 5, 100 + (i * rect_ht))
+            ctx.line_to(f[0] - 5, 100 + (i * rect_ht) + rect_ht / 2)
+            ctx.stroke()
+            ctx.move_to(
+                fields[last_field - 1][0] - 5, 100 + (i * rect_ht) + rect_ht / 2
+            )
+            ctx.line_to(560, 100 + (i * rect_ht) + rect_ht / 2)
+            ctx.stroke()
+            ctx.move_to(
+                fields[last_field - 1][0] - 2, 100 + (i * rect_ht) + 7 * (rect_ht / 8)
+            )
+            ctx.set_font_size(12)
+            ctx.show_text("Cumulative:")
+
+    def gen_single_player_scorecard(self, ctx, div, nrounds, meta, pidx):
+        player = div["players"]["persons"][pidx]
+        idtrunc = player["id"][: self.url_uniqueness_trunc]
+        qrcode_url = f"https://woogles.io{meta['metadata']['slug']}?es={idtrunc}"
+        if qrcode_url in self.qrcode_urls:
+            raise URLNotUniqueException()
+        self.place_qr_code(ctx, qrcode_url)
+        self.draw_name_and_tourney_header(ctx, player, pidx, meta["metadata"]["name"])
+
+        # header row
+        ctx.rectangle(25, 80, 535, 20)
+        ctx.stroke()
+
+        fields = [
+            (35, "Round"),
+            (85, "Opponent"),
+            (300, "Won"),
+            (335, "Lost"),
+            (370, "Your Score"),
+            (440, "Opp Score"),
+            (510, "Spread"),
+        ]
+
+        for field in fields:
+            ctx.move_to(field[0], 95)
+            ctx.show_text(field[1])
+
+        # header lines
+        for f in fields[1:]:
+            ctx.move_to(f[0] - 5, 80)
+            ctx.line_to(f[0] - 5, 100)
+
+        rect_ht = 40
+        if nrounds == 8:
+            rect_ht = 35
+        for i in range(nrounds):
+            self.draw_row(ctx, i, rect_ht, nrounds, fields)
+
+        # Draw known pairings
+        if self.show_opponents:
+            self.draw_known_pairings(ctx, div, pidx, rect_ht, nrounds)
+
+    def gen_scorecard(self, surface, ctx, div, nrounds, meta, p1, p2):
+        if p1 != p2:
+            for idx, pidx in enumerate([p1, p2]):
+                ctx.save()
+                ctx.translate(0, idx * 396)
+                self.gen_single_player_scorecard(ctx, div, nrounds, meta, pidx)
+                ctx.restore()
+            surface.show_page()  # Add a new page for the next scorecard
+        else:
+            ctx.save()
+            ctx.translate(0, 200)
+            self.gen_single_player_scorecard(ctx, div, nrounds, meta, p1)
+            ctx.restore()
+            surface.show_page()  # Add a new page for the next scorecard
+
+        surface.flush()
+
+    def gen_scorecards(self):
+        for divname, div in self.tourney["t"]["divisions"].items():
+            nrounds = len(div["round_controls"])
+            skip = 2 if nrounds <= 8 else 1
+            fname = f"{divname}_scorecards.pdf"
+            # 8.5 x 11 inches in points (612 x 792)
+            surface = cairo.PDFSurface(fname, 612, 792)
+            ctx = cairo.Context(surface)
+
+            for i in range(0, len(div["players"]["persons"]), skip):
+                self.gen_scorecard(
+                    surface,
+                    ctx,
+                    div,
+                    nrounds,
+                    self.tourney["meta"],
+                    i,
+                    i + skip - 1,
+                )
+            surface.finish()
 
 
-if __name__ == "__main__":
+@Gooey(
+    program_name="Tournament Manager for Woogles",
+    image_dir=local_resource_path("images"),
+)
+def main():
     parser = argparse.ArgumentParser(
         prog="gen",
         description="Generates physical scorecards for Woogles tournaments",
     )
 
-    parser.add_argument("tid_or_file")  # positional argument
-    parser.add_argument("-o", "--opponents", action="store_true")
+    parser.add_argument(
+        "tid_or_file",
+        help="Internal Tournament ID (see the bottom of Woogles Director tools)",
+    )  # positional argument
+    parser.add_argument(
+        "-o", "--opponents", action="store_true", help="Show opponents on scorecard"
+    )
+    parser.add_argument(
+        "-s",
+        "--seeds",
+        action="store_true",
+        help="Show player numbers/seeds on scorecard",
+    )
     args = parser.parse_args()
 
     tid = args.tid_or_file
@@ -288,4 +322,21 @@ if __name__ == "__main__":
     else:
         tourney = get_tournament(tid)
     # print(json.dumps(tourney))
-    gen_scorecards(tourney, args.opponents)
+    creator = ScorecardCreator(tourney, args.opponents, args.seeds)
+    success = False
+    while success is False:
+        try:
+            creator.gen_scorecards()
+        except URLNotUniqueException:
+            creator.reset()
+            creator.url_uniqueness_trunc += 1
+            print(
+                "Could not create unique URL, trying new trunc length",
+                creator.url_uniqueness_trunc,
+            )
+        else:
+            success = True
+
+
+if __name__ == "__main__":
+    main()
